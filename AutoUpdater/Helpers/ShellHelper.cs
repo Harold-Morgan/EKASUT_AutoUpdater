c82af1359bdd5027af85f1e0ac185a6541f776a7
Egor Gusarenko <volik46@gmail.com>
2020-06-23 18:17:54 +0300

    Create ShellHelper.cs

---
+++ AutoUpdater/Helpers/ShellHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoUpdater
+{
+    public static class ShellHelper
+    {
+        public static int ExecuteShellScript(string filePath)
+        {
+            if (filePath == null)
+                filePath = "/home/user/src/update_ekasut.sh";
+
+            FileInfo fileInfo = new FileInfo(filePath);
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = true;
+            startInfo.FileName = "/bin/bash";
+            startInfo.Arguments = $"\"{fileInfo.FullName}\"";
+            Process process = Process.Start(startInfo);
+
+            return process.ExitCode;
+        }
+    }
+}
